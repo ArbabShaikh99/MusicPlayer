50ce074f05a7e2e809278655d0935cb430caafec
Arbab <arbabshaikh901@gmail.com>
2025-09-10 16:54:12 +0500

    hi

--- app/src/main/java/com/example/musicplayerapp/presentationlayer/HelloWorld.kt
+++ app/src/main/java/com/example/musicplayerapp/presentationlayer/HelloWorld.kt
@@ -3,5 +3,6 @@ package com.example.musicplayerapp.presentationlayer
 data class HelloWorld (
     val message: String = "Hello, World!",
     val Status: String = "Success",
-    val code: Int = 200
+    val code: Int = 200,
+    val data: List<String> = listOf("Item1", "Item2", "Item3")
 )
