aa37ccb0b20337fc75e9569afd710d45c83defe8
Arbab <arbabshaikh901@gmail.com>
2025-09-11 10:09:45 +0500

    Done

--- app/src/main/java/com/example/musicplayerapp/presentationlayer/HelloWorld.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musicplayerapp.presentationlayer
-
-data class HelloWorld (
-    val message: String = "Hello, World!",
-    val Status: String = "Success",
-    val code: Int = 200,
-    val data: List<String> = listOf("Item1", "Item2", "Item3")
-)
